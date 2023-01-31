069b642ae264ee57c519befc3d478d75e888fdaa
Grzegorz Turek <g.turek@yahoo.com>
2023-01-31 21:03:33 +0100

    zadanie1

---
+++ main.py
@@ -0,0 +1,16 @@
+for i in range(5):
+    print("Spam") #cos tam cos tam
+ #   print("Eggs")
+
+"""
+duzy blokowy komentarz
+"""
+
+greetings = "Hello World!"
+greetings_type = type(greetings)
+print(greetings_type)
+
+age = 44
+name = "Greg"
+personalized_greetings = f"Witaj {name}, lat {age}!"
+print(personalized_greetings)
---
+++ zadanie_sery.py
@@ -0,0 +1,22 @@
+cheese_1 = "Roquefort"
+price_cheese_1 = 12.50
+cheese_2 = "Stilton"
+price_cheese_2 = 11.24
+cheese_3 = "Brie"
+price_cheese_3 = 9.30
+cheese_4 = "Gouda"
+price_cheese_4 = 8.55
+cheese_5 = "Edam"
+price_cheese_5 = 11
+cheese_6 = "Parmezan"
+price_cheese_6 = 16.50
+cheese_7 = "Mozzarella"
+price_cheese_7 = 14
+cheese_8 = "czechosłowacki ser z owczego mleka"
+price_cheese_8 = 122.32
+
+cheese_report = f"W dniu dzisiejszym udało się kupić ser {cheese_1} w cenie {price_cheese_1}zł, ser {cheese_2} w cenie {price_cheese_2}zł, " \
+                f"ser {cheese_3} w cenie {price_cheese_3}zł, ser {cheese_4} w cenie {price_cheese_4}zł, ser {cheese_5} w cenie {price_cheese_5}zł, " \
+                f"ser {cheese_6} w cenie {price_cheese_6}zł, ser {cheese_7} w cenie {price_cheese_7}zł " \
+                f"oraz słynny {cheese_8} w cenie {price_cheese_8}zł!!!"
+print(cheese_report)
