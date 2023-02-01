c1e0878155888b532a1bf311c7bc8771c5d01072
Grzegorz Turek <g.turek@yahoo.com>
2023-02-01 19:40:48 +0100

    gotowe zadania z petli

---
+++ petle_zadania.py
@@ -0,0 +1,21 @@
+# ZADANIE 1
+for i in range(5):
+        print("* " * 10)
+        print(" *" *10)
+
+#ZADANIE 2
+i = "*"
+n = 1
+for a in range(1):
+    for b in range(2):
+        print(i * 2)
+    for c in range(2):
+        print(i * 4)
+    for d in range(2):
+        print(i * 6)
+    for e in range(2):
+        print(i * 8)
+
+#ZADANIE 3
+for i in range(5,0,-1):
+    print("*" * i)
