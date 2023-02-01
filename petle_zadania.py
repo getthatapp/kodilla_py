d144daa44d0326e038110f8acde80224ea82e79a
Grzegorz Turek <g.turek@yahoo.com>
2023-02-01 21:26:22 +0100

    Update po review

--- petle_zadania.py
+++ petle_zadania.py
@@ -1,21 +1,13 @@
 # ZADANIE 1
-for i in range(5):
-        print("* " * 10)
-        print(" *" *10)
+# for i in range(5):
+#         print("* " * 10)
+#         print(" *" *10)
 
 #ZADANIE 2
-i = "*"
-n = 1
-for a in range(1):
-    for b in range(2):
-        print(i * 2)
-    for c in range(2):
-        print(i * 4)
-    for d in range(2):
-        print(i * 6)
-    for e in range(2):
-        print(i * 8)
+for i in range(2,9,2):
+    print("*" * i)
+    print("*" * i)
 
 #ZADANIE 3
-for i in range(5,0,-1):
-    print("*" * i)
+# for i in range(5,0,-1):
+#     print("*" * i)
