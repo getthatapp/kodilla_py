dfeae9e491fcdacbde0c0e7edbe1c4c65e74e0ea
Grzegorz Turek <g.turek@yahoo.com>
2023-02-01 21:09:51 +0100

    Squashed commit of the following:
    commit 7ae37c0112dfd58ea1ca609a260900e316fd1714
    Author: Grzegorz Turek <g.turek@yahoo.com>
    Date:   Wed Feb 1 21:08:58 2023 +0100
    
        bool done

---
+++ bool.py
@@ -0,0 +1,21 @@
+# raining = True
+# if raining:
+#     print("Wez parasol")
+# else:
+#     print("Nie bierz")
+
+# print(2 > 2)
+# print(2 < 2)
+# print(2 == 2)
+# print(2 >= 2)
+# print(2 <= 2)
+# print(2 != 2)
+
+available_cheese = "brie"
+if available_cheese == "edam":
+    print("kupuję edam")
+elif available_cheese == "brie":
+    print("wezme brie")
+else:
+    print("trudno, jednak gouda")
+
