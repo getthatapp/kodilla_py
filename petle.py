728f5a498e0d45af097cbd17e53e111ad3dd48b6
Grzegorz Turek <g.turek@yahoo.com>
2023-02-02 13:16:30 +0100

    gotowe zadania z petli (#2)
    * gotowe zadania z petli
    
    * Update po review
    
    * done

---
+++ petle.py
@@ -0,0 +1,12 @@
+# for i in range(5):
+#     print("Hello")
+
+# for i in range(1, 11):
+#     print(i)
+
+# word = "Szczebrzeszyn"
+# for i in word:
+#     print(i)
+
+for i in range(1, 6):
+     print(i*"*")
---
+++ petle_zadania.py
@@ -0,0 +1,13 @@
+# ZADANIE 1
+# for i in range(5):
+#         print("* " * 10)
+#         print(" *" *10)
+
+#ZADANIE 2
+for i in range(2,9,2):
+    print("*" * i)
+    print("*" * i)
+
+#ZADANIE 3
+# for i in range(5,0,-1):
+#     print("*" * i)
