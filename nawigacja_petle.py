8ba395f4a71de1b8628decfc33f86363d753a621
Grzegorz Turek <g.turek@yahoo.com>
2023-02-12 16:04:45 +0100

    postepy

---
+++ nawigacja_petle.py
@@ -0,0 +1,16 @@
+vegetables = ["burak", "ziemniak", "szczypior", "cebula"]
+
+for vegetable in vegetables:
+    print(vegetable)
+
+veggie_iterator = iter(vegetables)
+print(veggie_iterator)
+for vege in veggie_iterator:
+    print(vege)
+
+# veggie_iterator = iter(vegetables)
+# print(next(veggie_iterator))
+# print(next(veggie_iterator))
+# print(next(veggie_iterator))
+# print(next(veggie_iterator))
+# print(next(veggie_iterator))
