fbd0da7ab7257457a87574dc0f6d78f411afddb9
Grzegorz Turek <g.turek@yahoo.com>
2023-02-15 20:29:30 +0100

    koniec

--- nawigacja_petle.py
+++ nawigacja_petle.py
@@ -1,12 +1,12 @@
-vegetables = ["burak", "ziemniak", "szczypior", "cebula"]
+# vegetables = ["burak", "ziemniak", "szczypior", "cebula"]
 
-for vegetable in vegetables:
-    print(vegetable)
+# for vegetable in vegetables:
+#     print(vegetable)
 
-veggie_iterator = iter(vegetables)
-print(veggie_iterator)
-for vege in veggie_iterator:
-    print(vege)
+# veggie_iterator = iter(vegetables)
+# print(veggie_iterator)
+# for vege in veggie_iterator:
+#     print(vege)
 
 # veggie_iterator = iter(vegetables)
 # print(next(veggie_iterator))
@@ -14,3 +14,28 @@ for vege in veggie_iterator:
 # print(next(veggie_iterator))
 # print(next(veggie_iterator))
 # print(next(veggie_iterator))
+
+# shopping = [("buraki", 1.25), ("mleko", 4.0), ("chleb", 3.50), ("wino", 15)]
+
+# for product, price in shopping:
+#     print(product)
+
+# shopping_dict = dict(shopping)
+# print(shopping_dict)
+# for product in shopping_dict:
+#     print(product)
+# for product in shopping_dict.items():
+#     print(product)
+
+cities = {
+    "Niemcy": "Berlin",
+    "Czechy": "Praga",
+    "Słowacja": "Bratysława",
+    "Ukraina": "Kijów",
+    "Białoruś": "Mińsk"
+}
+
+for x, y in cities.items():
+    print(x, y)
+
+    
