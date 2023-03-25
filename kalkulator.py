f543541803da94fcd83818cdef2c9b99301175ee
Grzegorz Turek <g.turek@yahoo.com>
2023-03-25 16:42:01 +0100

    gotowe

--- kalkulator.py
+++ kalkulator.py
@@ -1,13 +1,25 @@
-choice = input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ')
-def calculator(a, b):
-    if choice == 1:
-        score = a + b
-    elif choice == 2:
-        score = a - b
-    elif choice == 3:
-        score = a * b
-    elif choice == 4:
-        score = a / b
-    else:
-        exit(1)
+choice = input('Podaj działanie, posługując się odpowiednią liczbą: [1] Dodawanie, [2] Odejmowanie, [3] Mnożenie, [4] Dzielenie: ')
+
+if choice == '1':
+    num1 = float(input("Podaj pierwszą liczbę: "))
+    num2 = float(input("Podaj drugą liczbę: "))
+    score = num1 + num2
+    print('Wynik:', score)
+elif choice == '2':
+    num1 = float(input("Podaj pierwszą liczbę: "))
+    num2 = float(input("Podaj drugą liczbę: "))
+    score = num1 - num2
+    print('Wynik:', score)
+elif choice == '3':
+    num1 = float(input("Podaj pierwszą liczbę: "))
+    num2 = float(input("Podaj drugą liczbę: "))
+    score = num1 * num2
+    print('Wynik:', score)
+elif choice == '4':
+    num1 = float(input("Podaj pierwszą liczbę: "))
+    num2 = float(input("Podaj drugą liczbę: "))
+    score = num1 / num2
+    print('Wynik:', score)
+else:
+    exit(1)
 
