c48aa5239a247ac1a0ce8a2f4b41bded3085b291
Grzegorz Turek <g.turek@yahoo.com>
2023-03-25 20:20:29 +0100

    poprawki

--- kalkulator.py
+++ kalkulator.py
@@ -1,25 +1,29 @@
-choice = input('Podaj działanie, posługując się odpowiednią liczbą: [1] Dodawanie, [2] Odejmowanie, [3] Mnożenie, [4] Dzielenie: ')
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename='logfile_calc.txt')
 
+choice = input('Podaj działanie, posługując się odpowiednią liczbą: [1] Dodawanie, [2] Odejmowanie, [3] Mnożenie, [4] Dzielenie: ')
 if choice == '1':
     num1 = float(input("Podaj pierwszą liczbę: "))
     num2 = float(input("Podaj drugą liczbę: "))
     score = num1 + num2
-    print('Wynik:', score)
+    logging.info('Wynik:', score)
 elif choice == '2':
     num1 = float(input("Podaj pierwszą liczbę: "))
     num2 = float(input("Podaj drugą liczbę: "))
     score = num1 - num2
-    print('Wynik:', score)
+    logging.info('Wynik:', score)
 elif choice == '3':
     num1 = float(input("Podaj pierwszą liczbę: "))
     num2 = float(input("Podaj drugą liczbę: "))
     score = num1 * num2
-    print('Wynik:', score)
+    logging.info('Wynik:', score)
 elif choice == '4':
     num1 = float(input("Podaj pierwszą liczbę: "))
     num2 = float(input("Podaj drugą liczbę: "))
     score = num1 / num2
-    print('Wynik:', score)
+    logging.info('Wynik:', score)
 else:
     exit(1)
 
+if __name__ == '__main__':
+    logging.info('Oto wynik działania: %s' % score)
