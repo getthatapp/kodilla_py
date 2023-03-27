80f0184601299f8e315a60504a8d14c5562a6bab
Grzegorz Turek <g.turek@yahoo.com>
2023-03-27 22:00:48 +0200

    rozwiniecie zadania z adresami

--- address_book.py
+++ address_book.py
@@ -1,5 +1,4 @@
 from faker import Faker
-
 fake = Faker()
 
 
@@ -14,17 +13,20 @@ class Contact:
     def __str__(self):
         return f'{self.first_name} {self.last_name}, {self.company}, {self.job}, {self.email}'
 
+if __name__ == '__main__':
 
-contacts = []
-for _ in range(5):
-    first_name = fake.first_name()
-    last_name = fake.last_name()
-    company = fake.company()
-    job = fake.job()
-    email = fake.email()
+    contacts = []
+    for _ in range(5):
+        first_name = fake.first_name()
+        last_name = fake.last_name()
+        company = fake.company()
+        job = fake.job()
+        email = fake.email()
 
-    contact = Contact(first_name, last_name, company, job, email)
-    contacts.append(contact)
+        contact = Contact(first_name, last_name, company, job, email)
+        contacts.append(contact)
+        contacts.sort(key=lambda x: x.first_name)
+        # contacts.sort(key=lambda x: x.last_name)
 
-for contact in contacts:
-    print(contact)
+    for contact in contacts:
+        print(contact)
--- obiekty.py
+++ obiekty.py
@@ -4,7 +4,45 @@ class Car:
         self.model_name = model_name
         self.top_speed = top_speed
         self.color = color
+    ###str###
+    def __str__(self):
+        return f'{self.color} {self.make} {self.model_name} '
+    ###repr###
+    def __repr__(self):
+        return f"Car(make={self.make} {self.model_name} {self.top_speed})"
+    ###eq###
+    def __eq__(self, other):
+        return (
+            self.make == other.make and
+            self.model_name == other.model_name and
+            self.top_speed == other.top_speed and
+            self.color == other.color
+        )
+    def __eq__(self, other):
+        return all(
+            (self.make == other.make,
+            self.model_name == other.model_name,
+            self.top_speed == other.top_speed,
+            self.color == other.color)
+        )
+    def __gt__(self, other):
+        return self.top_speed > other.top_speed
 
-mustang = Car(make='Ford', model_name='Mustang', top_speed=300, color='black')
-print(mustang)
-print(mustang.top_speed)
+car_one = Car(make='Ford', model_name='Mustang', top_speed=300, color='Black')
+car_two = Car(make="Ford", model_name="Mustang", top_speed=300, color="Black")
+car_three = Car(make="Ford", model_name="Mustang", top_speed=250, color="Yellow")
+
+car_1 = Car(make='Ford', model_name='Mustang', top_speed=250, color='Red')
+car_2 = Car(make='Ford', model_name='Fiesta', top_speed=200, color='Blue')
+car_3 = Car(make='Ferrari', model_name='Testarossa', top_speed=350, color='Black')
+cars = [car_1, car_2, car_3]
+by_speed = sorted(cars, key=lambda car: car.top_speed)
+by_make = sorted(cars, key=lambda car: car.make)
+print(by_speed)
+print(by_make)
+
+print(car_one)
+print(car_two)
+print(car_one == car_two) #eq
+print(car_one == car_three) #eq all
+print(car_one > car_three)
