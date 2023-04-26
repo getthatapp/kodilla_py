434113f2223900f38d0a96bad32dafc86213f3a1
Grzegorz Turek <g.turek@yahoo.com>
2023-04-26 12:11:56 +0200

    Squashed commit of the following:
    commit 0c1f15b228b615573c528ceb8a2f3c92a517a2cc
    Author: Grzegorz Turek <g.turek@yahoo.com>
    Date:   Tue Apr 25 20:44:57 2023 +0200
    
        update lokalizacji faker
    
    commit 8e3fbfbb8b739cdb23f3b36be24b426411a25d2f
    Author: Grzegorz Turek <g.turek@yahoo.com>
    Date:   Tue Apr 25 20:35:20 2023 +0200
    
        update zadania wizytowki
    
    commit 823e780994c2936f7abccfde66586978fda177eb
    Author: Grzegorz Turek <g.turek@yahoo.com>
    Date:   Mon Apr 3 20:43:24 2023 +0200
    
        tresc obiektow i dziedziczenia, plik pod rozszerzenie zadania z wizytowkami
    
    commit eabcde8d7bf2cb82d5e875529ded31ed4a25ad8e
    Author: Grzegorz Turek <g.turek@yahoo.com>
    Date:   Mon Apr 3 19:53:39 2023 +0200
    
        init

--- MODUL 7/address_book.py
+++ MODUL 7/address_book.py
@@ -1,31 +1,60 @@
 from faker import Faker
-fake = Faker()
+
+fake = Faker('pl_PL')
 
 
-class Contact:
-    def __init__(self, first_name, last_name, company, job, email):
+class BaseContact:
+    def __init__(self, first_name, last_name, phone, email):
         self.first_name = first_name
         self.last_name = last_name
-        self.company = company
-        self.job = job
+        self.phone = phone
         self.email = email
 
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}, {self.company}, {self.job}, {self.email}'
+    @property
+    def label_length(self):
+        return len(self.first_name + self.last_name)
+
+    def contact(self):
+        print(f"Wybieram numer {self.phone} i dzwonię do {self.first_name} {self.last_name}")
+
+
+class BusinessContact(BaseContact):
+    def __init__(self, first_name, last_name, phone, email, job, company, business_phone):
+        super().__init__(first_name, last_name, phone, email)
+        self.job = job
+        self.company = company
+        self.business_phone = business_phone
+
+    def contact(self):
+        print(f"Wybieram numer {self.business_phone} i dzwonię do {self.first_name} {self.last_name} z firmy {self.company}")
+
+
+def create_contacts(contact_type, quantity):
+    contacts = []
+    for _ in range(quantity):
+        first_name = fake.first_name()
+        last_name = fake.last_name()
+        phone = fake.phone_number()
+        email = fake.email()
+        job = fake.job()
+        company = fake.company()
+        business_phone = fake.phone_number()
+
+        if contact_type == "base":
+            contact = BaseContact(first_name, last_name, phone, email)
+        else:
+            contact = BusinessContact(first_name, last_name, phone, email, job, company, business_phone)
+
+        contacts.append(contact)
+
+    return contacts
 
 
 if __name__ == '__main__':
+    base_contacts = create_contacts("base", 5)
+    for contact in base_contacts:
+        contact.contact()
 
-    contacts = []
-    for _ in range(5):
-        first_name = fake.first_name()
-        last_name = fake.last_name()
-        company = fake.company()
-        job = fake.job()
-        email = fake.email()
-
-        contact = Contact(first_name, last_name, company, job, email)
-        contacts.append(contact)
-def contact():
-    print(f'Kontaktuję się z: {first_name} {last_name}, {job}, {email}')
-contact()
+    business_contacts = create_contacts("business", 5)
+    for contact in business_contacts:
+        contact.contact()
