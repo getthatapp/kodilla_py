cf6114fb1d47ba9337dd5e45d793298762a9a22e
Grzegorz Turek <g.turek@yahoo.com>
2023-05-15 23:04:37 +0200

    poprawka apki nbp

--- MODUL 9/nbp_app/env.sh
+++ MODUL 9/nbp_app/env.sh
@@ -1,2 +1,7 @@
 export FLASK_ENV=development
+<<<<<<< HEAD
+=======
+# shellcheck disable=SC2034
+FLASK_APP="nbp"
+>>>>>>> 87a6ff3 (poprawka apki nbp)
 flask run
--- MODUL 9/wtf_app/forms.py
+++ MODUL 9/wtf_app/forms.py
@@ -1,13 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
+<<<<<<< HEAD
 from wtforms.validators import DataRequired, Email
+=======
+from wtforms.validators import DataRequired
+>>>>>>> 87a6ff3 (poprawka apki nbp)
 
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
+<<<<<<< HEAD
 
 
 class EmailPasswordForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
+=======
+>>>>>>> 87a6ff3 (poprawka apki nbp)
