b0dbb89985e97fd13197a3cac4a00a8829ef32b1
Grzegorz Turek <g.turek@yahoo.com>
2023-05-16 21:52:33 +0200

    json api

---
+++ MODUL 9/todos_api/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, jsonify, abort, make_response, request
+from models import todos
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "nininini"
+
+@app.route("/api/v1/todos/", methods=["GET"])
+def todos_list_api_v1():
+    return jsonify(todos.all())
+
+@app.route("/api/v1/todos/<int:todo_id>", methods=["GET"])
+def get_todo(todo_id):
+    todo = todos.get(todo_id)
+    if not todo:
+        abort(404)
+    return jsonify({"todo": todo})
+
+
+@app.route("/api/v1/todos", methods=["POST"])
+def create_todo():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    todo = {
+        'id': todos.all()[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    todos.create(todo)
+    return jsonify({'todo': todo}), 201
+
+
+@app.route("/api/v1/todos/<int:todo_id", methods=["PUT"])
+def update_todo(todo_id):
+    todo = todos.get(todo_id)
+    if not todo:
+        abort(404)
+    if not request.json:
+        abort(400)
+    data = request.json
+
+
+@app.route("/api/v1/todos/<int:todo_id>", methods=["DELETE"])
+def delete_todo(todo_id):
+    result = todos.delete(todo_id)
+    if not result:
+        abort(404)
+    return jsonify({'result': result})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found', 'status_code': 404}), 404)
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request', 'status_code': 400}), 400)
+
+if __name__ == "__main__":
+    app.run(debug=True)
