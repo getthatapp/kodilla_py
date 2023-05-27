8d6672606cf5b1965aae03b5e083e0aab19dae70
Grzegorz Turek <g.turek@yahoo.com>
2023-05-27 22:55:05 +0200

    14.2 zadanie

---
+++ MODUL 14/testy.py
@@ -0,0 +1,12 @@
+
+def add(x, y):
+    return x + y
+
+def test_add():
+    result = add(2, 2)
+    assert result == 4
+
+def test_add_negative():
+    result = add(-2, -2)
+    assert result == -4
+
